d91df78be739e06420e37f5e3a79c23aa3e6536a
Salvio Furbino <salvio@localhost.localdomain>
2025-01-27 18:01:29 -0300

    Atualizacao automática

--- src/main/java/br/org/coletivoJava/integracoes/restClickup/implementacao/IntegracaoRestClickupTimeListar.java
+++ src/main/java/br/org/coletivoJava/integracoes/restClickup/implementacao/IntegracaoRestClickupTimeListar.java
@@ -5,16 +5,23 @@ import com.super_bits.Super_Bits.intClickup.regras_de_negocio_e_controller.FabIn
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.implementacao.AcaoApiIntegracaoComOauthAbstrato;
 import com.super_bits.modulosSB.SBCore.integracao.libRestClient.api.FabTipoAgenteClienteApi;
 import com.super_bits.modulosSB.SBCore.modulos.objetos.registro.Interfaces.basico.ItfUsuario;
+import java.util.Map;
 
 @InfoIntegracaoRestClickupTimes(tipo = FabIntRestClickupTimes.TIME_LISTAR)
 public class IntegracaoRestClickupTimeListar
-		extends
-			AcaoApiIntegracaoComOauthAbstrato {
+        extends
+        AcaoApiIntegracaoComOauthAbstrato {
 
-	public IntegracaoRestClickupTimeListar(
-			final FabTipoAgenteClienteApi pTipoAgente,
-			final ItfUsuario pUsuario, final java.lang.Object... pParametro) {
-		super(FabIntRestClickupTimes.TIME_LISTAR, pTipoAgente, pUsuario,
-				pParametro);
-	}
+    public IntegracaoRestClickupTimeListar(
+            final FabTipoAgenteClienteApi pTipoAgente,
+            final ItfUsuario pUsuario, final java.lang.Object... pParametro) {
+        super(FabIntRestClickupTimes.TIME_LISTAR, pTipoAgente, pUsuario,
+                pParametro);
+    }
+
+    @Override
+    public Map<String, String> gerarCabecalho() {
+        return super.gerarCabecalho(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
 }
